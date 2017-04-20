0ae6482eb62bc49a862d1c36e0c6a0694c5ec668
koheishoji <koheishoji913@gmail.com>
2017-04-20 22:40:53 +0900

    create view

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,13 @@
 class MessagesController < ApplicationController
   def index
+    @notification = "create chat group"
+    @user = {name: "ryo"}
+    @groups = [{name: "god", message: "god of god"},
+               {name: "aaaaaaaaa", message: "aaaaaaa"},
+               {name: "sample", message: "no message"},]
+    @group = {name: "sample", menbers: ["seo", "neko"]}
+    @messages = [{name: "seo", date: "2016/09/21 06:16:55", body: "Hello world!"},
+      {name: "neko", date: "2016/09/22 06:16:55", body: "Good bye world!"}]
+    @chat = ""
   end
 end
