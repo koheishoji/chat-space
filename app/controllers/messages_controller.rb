ea9108fde9d905b93182be421e05fd83b8614a91
koheishoji <koheishoji913@gmail.com>
2017-04-21 19:56:59 +0900

    view create

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,7 +7,8 @@ class MessagesController < ApplicationController
                {name: "sample", message: "no message"},]
     @group = {name: "sample", menbers: ["seo", "neko"]}
     @messages = [{name: "seo", date: "2016/09/21 06:16:55", body: "Hello world!"},
-      {name: "neko", date: "2016/09/22 06:16:55", body: "Good bye world!"}]
+      {name: "neko", date: "2016/09/22 06:16:55", body: "Good bye world!"},
+      {name: "neko", date: "2016/09/23 06:16:55", body: "See you later!"}]
     @chat = ""
   end
 end
