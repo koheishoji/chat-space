2e3c53ca3d6f569986d8bed67d6eafbe85ebf6bc
koheishoji <koheishoji913@gmail.com>
2017-05-14 19:02:19 +0900

    edit user name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+  def edit
+    @user = current_user
+  end
+
+  def update
+    user = current_user
+    user.update update_user_params
+    redirect_to controller: :messages, action: :index
+  end
+
+  protected
+  def update_user_params
+    params.require(:user).permit(:name)
+  end
 end
