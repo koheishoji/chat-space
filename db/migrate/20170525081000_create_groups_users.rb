f0391821028d6fc8f52a63fc4889fcf7b84bcc5b
koheishoji <koheishoji913@gmail.com>
2017-05-25 17:19:28 +0900

    add foreign_key from groups_users

--- db/migrate/20170525081000_create_groups_users.rb
+++ db/migrate/20170525081000_create_groups_users.rb
@@ -1,8 +1,8 @@
 class CreateGroupsUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :groups_users do |t|
-      t.references :user, index: true
-      t.references :group, index: true
+      t.references :user, foreign_key: true
+      t.references :group, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -45,4 +45,6 @@ ActiveRecord::Schema.define(version: 20170525081000) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "groups_users", "groups"
+  add_foreign_key "groups_users", "users"
 end
