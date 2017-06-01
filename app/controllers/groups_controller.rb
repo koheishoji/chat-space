0bbdf969b296e4e3dd87bc6fab8a735de9f1c5a1
koheishoji <koheishoji913@gmail.com>
2017-06-01 14:35:56 +0900

    change update_attributes to update method

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -22,7 +22,7 @@ class GroupsController < ApplicationController
 
   def update
     @group = Group.find(params[:id])
-    if @group.update_attributes(group_params)
+    if @group.update(group_params)
       redirect_to group_messages_path(params[:id]), notice: "グループが変更されました"
     else
       render :edit
