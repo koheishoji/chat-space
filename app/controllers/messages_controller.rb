5c5d480bb5f7c4e23ecba5d1b4ae1926afe2467a
koheishoji <koheishoji913@gmail.com>
2017-06-04 13:25:49 +0900

    bug fixed

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,7 @@
 class MessagesController < ApplicationController
   def index
     @groups = current_user.groups
-    @mgroup = Group.find(params[:group_id])
+    @group = Group.find(params[:group_id])
     @messages = [{name: "seo", date: "2016/09/21 06:16:55", body: "Hello world!"},
       {name: "neko", date: "2016/09/22 06:16:55", body: "Good bye world!"},
       {name: "neko", date: "2016/09/23 06:16:55", body: "See you later!"}]
