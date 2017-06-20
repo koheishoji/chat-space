97f86fe1082eae7df0d07fc243729e3a4983935e
koheishoji <koheishoji913@gmail.com>
2017-06-20 21:33:27 +0900

    add validate to foreign key

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,4 +5,6 @@ class Message < ApplicationRecord
   mount_uploader :image, ImageUploader
 
   validates :body, presence: true, if: "image.blank?"
+  validates :group_id, presence: true
+  validates :user_id, presence: true
 end
