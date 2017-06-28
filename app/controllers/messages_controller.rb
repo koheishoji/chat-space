aee2d0e20e9cf24ebabff376a03fa5018707bc57
koheishoji <koheishoji913@gmail.com>
2017-06-28 21:51:18 +0900

    add json to messages#create

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,7 +9,10 @@ class MessagesController < ApplicationController
   def create
     @message = Message.new(message_params)
     if @message.save
-      redirect_to group_messages_path(params[:group_id]), notice:  "メッセージが送信されました"
+      respond_to do |format|
+        format.html { redirect_to group_messages_path(params[:group_id]), notice:  "メッセージが送信されました" }
+        format.json
+      end
     else
       redirect_to group_messages_path(params[:group_id]), alert: "メッセージを入力してください"
     end
