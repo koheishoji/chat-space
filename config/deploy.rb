cda22b443ddf4ba32d5753051d8eb3d85d63c668
koheishoji <koheishoji913@gmail.com>
2017-07-22 17:31:00 +0900

    change github username

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.8.2'
 
 set :application, 'chat-space'
-set :repo_url,  'git@github.com:koheishoji913/chat-space.git'
+set :repo_url,  'git@github.com:koheishoji/chat-space.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
